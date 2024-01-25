d5ceeecbfa440258cc96917aee0b64c0239d25a9
ilja26041994 <moskaljow@gmail.com>
2024-01-25 19:50:50 +0300

    create models

---
+++ autopark/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class CarType(models.Model):
+    name = models.CharField(max_length=255)
+
+class CarBrand(models.Model):
+    name = models.CharField(max_length=255)
+
+class Car(models.Model):
+    car_number = models.CharField(max_length=255)
+    car_type = models.CharField(CarType, on_delete=models.SET_NULL, null=True)
+    car_brand = models.CharField(CarBrand, on_delete=models.SET_NULL, null=True)
+    is_electric = models.BooleanField(default=False)
+    year = models.IntegerField()
+
+class ParkingSlot(models.Model):
+    is_free = models.BooleanField(default=True)
+    number = models.IntegerField()
+    car = models.OneToOneField(Car, on_delete=models.SET_NULL, null=True)
+
+class Parking(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField(null=True)
+    parking_slots = models.ForeignKey(ParkingSlot, on_delete=models.SET_NULL, null=True)
+
+
